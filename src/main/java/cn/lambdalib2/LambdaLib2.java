f8c2e0264f70be446977dd0afc58f833023ccfe5
KS <kskun@qq.com>
2018-02-18 23:00:15 +0800

    Add debug flag, logger and version property

--- src/main/java/cn/lambdalib2/LambdaLib2.java
+++ src/main/java/cn/lambdalib2/LambdaLib2.java
@@ -2,24 +2,37 @@ package cn.lambdalib2;
 
 import cn.lambdalib2.registry.RegistryMod;
 import cn.lambdalib2.util.DebugDraw;
-import net.minecraft.init.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import org.apache.logging.log4j.Logger;
 
 @RegistryMod
 @Mod(modid = LambdaLib2.MODID, version = LambdaLib2.VERSION)
 public class LambdaLib2
 {
     public static final String MODID = "lambdalib2";
-    public static final String VERSION = "0.1";
-    
+    public static final String VERSION = "@VERSION@";
+
+    /**
+     * Whether we are in development (debug) mode.
+     */
+    public static final boolean DEBUG = VERSION.startsWith("@");
+
+    public static Logger log;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        log = event.getModLog();
+    }
+
     @EventHandler
     public void init(FMLInitializationEvent event) {
-
+        if(DEBUG) log.info("LambdaLib2 is running in development mode.");
     }
 
     @EventHandler
