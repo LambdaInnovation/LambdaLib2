31e8f44877096aad131dcddf6c3d3e1b6be14662
Paindar <1944798085@qq.com>
2018-08-18 03:19:42 +0800

    add method rotateAroundZ(Vec3d, float) and setMotion(Entity, Vec3d) to VecUtils

--- src/main/java/cn/lambdalib2/util/VecUtils.java
+++ src/main/java/cn/lambdalib2/util/VecUtils.java
@@ -191,4 +191,20 @@ public class VecUtils {
     public static Vec3d lookingPos(Entity e, double range){
         return add(e.getPositionVector(), multiply(e.getLookVec(), range));
     }
+
+    public static Vec3d rotateAroundZ(Vec3d v, float p_72446_1_)
+    {
+        float f1 = MathHelper.cos(p_72446_1_);
+        float f2 = MathHelper.sin(p_72446_1_);
+        double d0 = v.x * (double)f1 + v.y * (double)f2;
+        double d1 = v.y * (double)f1 - v.x * (double)f2;
+        double d2 = v.z;
+        return new Vec3d(d0, d1, d2);
+    }
+
+    public static void setMotion(Entity e, Vec3d motion){
+        e.motionX = motion.x;
+        e.motionY = motion.y;
+        e.motionZ = motion.z;
+    }
 }
