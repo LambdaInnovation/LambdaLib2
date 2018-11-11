d019fc7eaf5b6c0c07b7772f50da5c95461e40f1
weathfold <weathfold@li-dev.cn>
2018-11-12 01:05:31 +0800

    RenderUtils: Add back drawEquippedItem

--- src/main/java/cn/lambdalib2/util/RenderUtils.java
+++ src/main/java/cn/lambdalib2/util/RenderUtils.java
@@ -3,6 +3,7 @@ package cn.lambdalib2.util;
 import java.lang.reflect.Field;
 
 import cn.lambdalib2.LambdaLib2;
+import cn.lambdalib2.render.legacy.Tessellator;
 import net.minecraft.util.math.Vec3d;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.glu.GLU;
@@ -104,72 +105,77 @@ public class RenderUtils {
     
     private static void drawEquippedItem(double w, ResourceLocation front, ResourceLocation back, 
             double u1, double v1, double u2, double v2, boolean faceOnly) {
-        Debug.TODO();
-//        Vec3d a1 = vec(0, 0, w),
-//            a2 = vec(1, 0, w),
-//            a3 = vec(1, 1, w),
-//            a4 = vec(0, 1, w),
-//            a5 = vec(0, 0, -w),
-//            a6 = vec(1, 0, -w),
-//            a7 = vec(1, 1, -w),
-//            a8 = vec(0, 1, -w);
-//
-//        Minecraft mc = Minecraft.getMinecraft();
-//
-//        GL11.glPushMatrix();
-//
-//        RenderUtils.loadTexture(back);
-//        t.startDrawingQuads();
-//        t.setNormal(0.0F, 0.0F, 1.0F);
-//        addVertex(a1, u2, v2);
-//        addVertex(a2, u1, v2);
-//        addVertex(a3, u1, v1);
-//        addVertex(a4, u2, v1);
-//        t.draw();
-//
-//        RenderUtils.loadTexture(front);
-//        t.startDrawingQuads();
-//        t.setNormal(0.0F, 0.0F, -1.0F);
-//        addVertex(a8, u2, v1);
-//        addVertex(a7, u1, v1);
-//        addVertex(a6, u1, v2);
-//        addVertex(a5, u2, v2);
-//        t.draw();
-//
-//        int var7;
-//        float var8;
-//        double var9;
-//        float var10;
-//
-//        /*
-//         * Gets the width/16 of the currently bound texture, used to fix the
-//         * side rendering issues on textures != 16
-//         */
-//        int tileSize = 32;
-//        float tx = 1.0f / (32 * tileSize);
-//        float tz = 1.0f / tileSize;
-//
-//        if(!faceOnly) {
-//            t.startDrawingQuads();
-//            t.setNormal(-1.0F, 0.0F, 0.0F);
-//            for (var7 = 0; var7 < tileSize; ++var7) {
-//                var8 = (float) var7 / tileSize;
-//                var9 = u2 - (u2 - u1) * var8 - tx;
-//                var10 = 1.0F * var8;
-//                t.addVertexWithUV(var10, 0.0D, -w, var9, v2);
-//                t.addVertexWithUV(var10, 0.0D, w, var9, v2);
-//                t.addVertexWithUV(var10, 1.0D, w, var9, v1);
-//                t.addVertexWithUV(var10, 1.0D, -w, var9, v1);
-//
-//                t.addVertexWithUV(var10, 1.0D, w, var9, v1);
-//                t.addVertexWithUV(var10, 0.0D, w, var9, v2);
-//                t.addVertexWithUV(var10, 0.0D, -w, var9, v2);
-//                t.addVertexWithUV(var10, 1.0D, -w, var9, v1);
-//            }
-//            t.draw();
-//        }
-//
-//        GL11.glPopMatrix();
+        Tessellator t = Tessellator.instance;
+        Vec3d a1 = new Vec3d(0, 0, w),
+            a2 = new Vec3d(1, 0, w),
+            a3 = new Vec3d(1, 1, w),
+            a4 = new Vec3d(0, 1, w),
+            a5 = new Vec3d(0, 0, -w),
+            a6 = new Vec3d(1, 0, -w),
+            a7 = new Vec3d(1, 1, -w),
+            a8 = new Vec3d(0, 1, -w);
+
+        Minecraft mc = Minecraft.getMinecraft();
+
+        GL11.glPushMatrix();
+
+        RenderUtils.loadTexture(back);
+        t.startDrawingQuads();
+        t.setNormal(0.0F, 0.0F, 1.0F);
+        addVertex(a1, u2, v2);
+        addVertex(a2, u1, v2);
+        addVertex(a3, u1, v1);
+        addVertex(a4, u2, v1);
+        t.draw();
+
+        RenderUtils.loadTexture(front);
+        t.startDrawingQuads();
+        t.setNormal(0.0F, 0.0F, -1.0F);
+        addVertex(a8, u2, v1);
+        addVertex(a7, u1, v1);
+        addVertex(a6, u1, v2);
+        addVertex(a5, u2, v2);
+        t.draw();
+
+        int var7;
+        float var8;
+        double var9;
+        float var10;
+
+        /*
+         * Gets the width/16 of the currently bound texture, used to fix the
+         * side rendering issues on textures != 16
+         */
+        int tileSize = 32;
+        float tx = 1.0f / (32 * tileSize);
+        float tz = 1.0f / tileSize;
+
+        if(!faceOnly) {
+            t.startDrawingQuads();
+            t.setNormal(-1.0F, 0.0F, 0.0F);
+            for (var7 = 0; var7 < tileSize; ++var7) {
+                var8 = (float) var7 / tileSize;
+                var9 = u2 - (u2 - u1) * var8 - tx;
+                var10 = 1.0F * var8;
+                t.addVertexWithUV(var10, 0.0D, -w, var9, v2);
+                t.addVertexWithUV(var10, 0.0D, w, var9, v2);
+                t.addVertexWithUV(var10, 1.0D, w, var9, v1);
+                t.addVertexWithUV(var10, 1.0D, -w, var9, v1);
+
+                t.addVertexWithUV(var10, 1.0D, w, var9, v1);
+                t.addVertexWithUV(var10, 0.0D, w, var9, v2);
+                t.addVertexWithUV(var10, 0.0D, -w, var9, v2);
+                t.addVertexWithUV(var10, 1.0D, -w, var9, v1);
+            }
+            t.draw();
+        }
+
+        GL11.glPopMatrix();
+    }
+
+    private static void addVertex(Vec3d vec, double u, double v) {
+        Tessellator t = Tessellator.instance;
+        t.addVertexWithUV(vec.x, vec.y, vec.z, u, v);
     }
     
     public static void renderOverlayEquip(ResourceLocation src) {
@@ -315,7 +321,7 @@ public class RenderUtils {
             smcPresent = true;
             Debug.log("LambdaLib SMC support successfully initialized.");
         } catch(Exception e) {
-            Debug.error("LambdaLib SMC support isn't initialized.", e);
+            Debug.error("LambdaLib SMC support isn't initialized.");
             smcPresent = false;
         }
         
