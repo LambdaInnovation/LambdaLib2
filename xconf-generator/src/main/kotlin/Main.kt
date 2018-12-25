5c4556deb5245487064ddfc1d8ef66db6297f3fe
weathfold <weathfold@li-dev.cn>
2018-12-25 10:55:12 +0800

    xconf: Support multiple item/block config files

--- xconf-generator/src/main/kotlin/Main.kt
+++ xconf-generator/src/main/kotlin/Main.kt
@@ -19,29 +19,22 @@ object Main {
         val adv: Array<String>
     )
 
+    class ImportItemConfig(val file: String, val classPath: String, val classAnnotations: Array<String>)
+
     class ImportConfig(
         val srcDir: String,
         val resourcesDir: String,
         val locPrefix: String,
         val domain: String,
 
-        val itemsDataFile: String,
-        val itemsClassPath: String,
-
-        val blocksDataFile: String,
-        val blocksClassPath: String,
+        val itemConfigs: List<ImportItemConfig>,
+        val blockConfigs: List<ImportItemConfig>,
 
         val advsDataFile: String,
         val advsClassPath: String,
 
         val additionalImports: AdditionalImports
     ) {
-        fun getItemsPackageName() = itemsClassPath.substringBeforeLast('.')
-        fun getItemsClassName() = itemsClassPath.substringAfterLast('.')
-
-        fun getBlocksPackageName() = blocksClassPath.substringBeforeLast('.')
-        fun getBlocksClassName() = blocksClassPath.substringAfterLast('.')
-
         fun getAdvsPackageName() = advsClassPath.substringBeforeLast('.')
         fun getAdvsClassName() = advsClassPath.substringAfterLast('.')
     }
@@ -167,188 +160,201 @@ object Main {
     }
 
     private fun writeItems(ctx: BaseContext) {
-        val rawItems = ConfigFactory.parseFile(File(ctx.rootDir, ctx.config.itemsDataFile))
-        val itemBase = rawItems.getValue("_base")
-        val items = rawItems
-            .root()
-            .filter { !it.key.startsWith("_") }
-            .map { (id, elem) ->
-                val obj = (elem as ConfigObject).toConfig().withFallback(itemBase)
-                ItemMetadata(
-                    id = id,
-                    baseClass = obj.getStringOrDefault("baseClass", "net.minecraft.item.Item"),
-                    ctorArgs = obj.getStringOrDefault("ctorArgs", ""),
-                    maxStackSize = obj.getIntOrNull("maxStackSize"),
-                    maxDamage = obj.getIntOrNull("maxDamage"),
-                    creativeTab = obj.getStringOrNull("creativeTab"),
-                    init = obj.getStrArrOrNull("init"),
-                    initAfterRegistry = obj.getStrArrOrNull("initAfterRegistry"),
+        ctx.config.itemConfigs.forEach {
+            val rawItems = ConfigFactory.parseFile(File(ctx.rootDir, it.file))
+            val itemBase = rawItems.getConfigOrNull("_base")
+            val items = rawItems
+                .root()
+                .filter { !it.key.startsWith("_") }
+                .map { (id, elem) ->
+                    val obj = (elem as ConfigObject).toConfig().let { x ->
+                        if (itemBase != null)  x.withFallback(itemBase) else x
+                    }
+                    ItemMetadata(
+                        id = id,
+                        baseClass = obj.getStringOrDefault("baseClass", "net.minecraft.item.Item"),
+                        ctorArgs = obj.getStringOrDefault("ctorArgs", ""),
+                        maxStackSize = obj.getIntOrNull("maxStackSize"),
+                        maxDamage = obj.getIntOrNull("maxDamage"),
+                        creativeTab = obj.getStringOrNull("creativeTab"),
+                        init = obj.getStrArrOrNull("init"),
+                        initAfterRegistry = obj.getStrArrOrNull("initAfterRegistry"),
 
-                    generateModel = obj.getBooleanOrDefault("generateModel", true),
-                    model = obj.getConfigOrNull("model"),
-                    extModels = obj.getConfigOrNull("extModels"),
-                    modelBindings = obj.getConfigOrNull("modelBindings")?.root()
-                        ?.map { it.key.toInt() to it.value.unwrapped() as String }?.toMap() ?:
-                    mapOf(0 to "${ctx.config.domain}:$id")
-                )
+                        generateModel = obj.getBooleanOrDefault("generateModel", true),
+                        model = obj.getConfigOrNull("model"),
+                        extModels = obj.getConfigOrNull("extModels"),
+                        modelBindings = obj.getConfigOrNull("modelBindings")?.root()
+                            ?.map { it.key.toInt() to it.value.unwrapped() as String }?.toMap() ?:
+                        mapOf(0 to "${ctx.config.domain}:$id")
+                    )
+                }
+                .sortedBy { it.id }
+
+            println("Writing item models...")
+            for (item in items) if (item.generateModel) {
+                val modelJsonPath = File(ctx.assetsRootDir, "models/item/${item.id}.json")
+                val modelJsonStr = if (item.model != null) {
+                    val obj: JsonObject = gson.fromJson(item.model.root().render(ConfigRenderOptions.concise()))
+                    obj.put(jsonMark)
+                    obj
+                } else {
+                    jsonObject(
+                        jsonMark,
+                        "parent" to "item/generated",
+                        "textures" to jsonObject("layer0" to "${ctx.config.domain}:items/${item.id}")
+                    )
+                }.toString()
+
+                modelJsonPath.parentFile.mkdirs()
+                modelJsonPath.writeText(modelJsonStr)
             }
-            .sortedBy { it.id }
 
-        println("Writing item models...")
-        for (item in items) if (item.generateModel) {
-            val modelJsonPath = File(ctx.assetsRootDir, "models/item/${item.id}.json")
-            val modelJsonStr = if (item.model != null) {
-                val obj: JsonObject = gson.fromJson(item.model.root().render(ConfigRenderOptions.concise()))
-                obj.put(jsonMark)
-                obj
-            } else {
-                jsonObject(
-                    jsonMark,
-                    "parent" to "item/generated",
-                    "textures" to jsonObject("layer0" to "${ctx.config.domain}:items/${item.id}")
-                )
-            }.toString()
-
-            modelJsonPath.parentFile.mkdirs()
-            modelJsonPath.writeText(modelJsonStr)
-        }
-
-        println("Writing item ext models...")
-        for (item in items) {
-            if (item.extModels != null) {
-                for (m in item.extModels.root()) {
-                    val path = File(ctx.assetsRootDir, "models/item/${m.key}.json")
-                    val conf = (m.value as ConfigObject).withValue(jsonMark.first, ConfigValueFactory.fromAnyRef(jsonMark.second))
-                    path.writeText(conf.render(ConfigRenderOptions.concise()))
+            println("Writing item ext models...")
+            for (item in items) {
+                if (item.extModels != null) {
+                    for (m in item.extModels.root()) {
+                        val path = File(ctx.assetsRootDir, "models/item/${m.key}.json")
+                        val conf = (m.value as ConfigObject).withValue(jsonMark.first, ConfigValueFactory.fromAnyRef(jsonMark.second))
+                        path.writeText(conf.render(ConfigRenderOptions.concise()))
+                    }
                 }
             }
-        }
 
-        println("Writing item class...")
-        run {
-            val itemClassFile = File(ctx.srcDir, ctx.config.itemsClassPath.replace('.', '/') + ".java")
-            itemClassFile.parentFile.mkdirs()
-
-            val s = renderTemplate("itemClassTemplate.java", mapOf(
-                "date" to dateFormat.format(Date()),
-                "config" to ctx.config,
-                "items" to items
-            ))
-            itemClassFile.writeText(s)
+            println("Writing item class...")
+            run {
+                val itemClassFile = File(ctx.srcDir, it.classPath.replace('.', '/') + ".java")
+                itemClassFile.parentFile.mkdirs()
+                val s = renderTemplate("itemClassTemplate.java", mapOf(
+                    "date" to dateFormat.format(Date()),
+                    "config" to ctx.config,
+                    "items" to items,
+                    "annotations" to it.classAnnotations,
+                    "packageName" to it.classPath.substringBeforeLast('.'),
+                    "className" to it.classPath.substringAfterLast('.')
+                ))
+                itemClassFile.writeText(s)
+            }
         }
     }
 
     private fun writeBlocks(ctx: BaseContext) {
-        val rawBlocks = ConfigFactory.parseFile(File(ctx.rootDir, ctx.config.blocksDataFile))
-        val blockBase = rawBlocks.getValue("_base")
-        val blocks = rawBlocks
-            .root()
-            .filter { !it.key.startsWith("_") }
-            .map { (id, elem) ->
-                val obj = (elem as ConfigObject).toConfig().withFallback(blockBase)
-                val item = obj.getConfigOrNull("item") ?: ConfigFactory.empty()
-                BlockMetadata(
-                    id,
-                    obj.getStringOrDefault("baseClass", "net.minecraft.block.Block"),
-                    obj.getStringOrDefault("ctorArgs", ""),
-                    obj.getStringOrNull("creativeTab"),
-                    obj.getStrArrOrNull("init"),
-                    obj.getBooleanOrDefault("hasItemBlock", true),
-                    BlockItemMetadata(
-                        item.getStringOrDefault("type", "ItemBlock"),
-                        item.getBooleanOrDefault("generateModel", true),
-                        item.getConfigOrNull("model")),
+        for (cfg in ctx.config.blockConfigs) {
+            val rawBlocks = ConfigFactory.parseFile(File(ctx.rootDir, cfg.file))
+            val blockBase = rawBlocks.getConfigOrNull("_base")
+            val blocks = rawBlocks
+                .root()
+                .filter { !it.key.startsWith("_") }
+                .map { (id, elem) ->
+                    val obj = (elem as ConfigObject).toConfig().let {
+                        if (blockBase != null) it.withFallback(blockBase) else it
+                    }
+                    val item = obj.getConfigOrNull("item") ?: ConfigFactory.empty()
+                    BlockMetadata(
+                        id,
+                        obj.getStringOrDefault("baseClass", "net.minecraft.block.Block"),
+                        obj.getStringOrDefault("ctorArgs", ""),
+                        obj.getStringOrNull("creativeTab"),
+                        obj.getStrArrOrNull("init"),
+                        obj.getBooleanOrDefault("hasItemBlock", true),
+                        BlockItemMetadata(
+                            item.getStringOrDefault("type", "ItemBlock"),
+                            item.getBooleanOrDefault("generateModel", true),
+                            item.getConfigOrNull("model")),
 
-                    generateModel = obj.getBooleanOrDefault("generateModel", true),
-                    model = obj.getConfigOrNull("model"),
-                    extModels = obj.getConfigOrNull("extModels"),
-                    blockStates = obj.getConfigOrNull("blockStates")
-                )
-            }
-            .sortedBy { it.id }
-
-        val blocksWithItemBlock = blocks.filter { it.hasItemBlock }
-
-        println("Writing block models...")
-        for (block in blocks) if (block.generateModel) {
-            val modelJsonPath = File(ctx.assetsRootDir, "models/block/${block.id}.json")
-            val modelJson = when {
-                block.model != null -> {
-                    block.model.toJsonObject()
+                        generateModel = obj.getBooleanOrDefault("generateModel", true),
+                        model = obj.getConfigOrNull("model"),
+                        extModels = obj.getConfigOrNull("extModels"),
+                        blockStates = obj.getConfigOrNull("blockStates")
+                    )
                 }
-                else -> {
+                .sortedBy { it.id }
+
+            val blocksWithItemBlock = blocks.filter { it.hasItemBlock }
+
+            println("Writing block models...")
+            for (block in blocks) if (block.generateModel) {
+                val modelJsonPath = File(ctx.assetsRootDir, "models/block/${block.id}.json")
+                val modelJson = when {
+                    block.model != null -> {
+                        block.model.toJsonObject()
+                    }
+                    else -> {
+                        jsonObject(
+                            "parent" to "block/cube_all",
+                            "textures" to jsonObject(
+                                "all" to "${ctx.config.domain}:blocks/${block.id}"
+                            )
+                        )
+                    }
+                }
+                modelJson.put(jsonMark)
+                modelJsonPath.parentFile.mkdirs()
+                modelJsonPath.writeText(modelJson.toString())
+            }
+
+            println("Writing block item models...")
+            for (block in blocksWithItemBlock) if (block.itemBlock.generateModel) {
+                val modelJsonPath = File(ctx.assetsRootDir, "models/item/${block.id}.json")
+                val modelJsonStr = if (block.itemBlock.model != null) {
+                    val obj: JsonObject = gson.fromJson(block.itemBlock.model.root().render(ConfigRenderOptions.concise()))
+                    obj[jsonMark.first] = jsonMark.second
+                    obj.toString()
+                } else {
                     jsonObject(
-                        "parent" to "block/cube_all",
-                        "textures" to jsonObject(
-                            "all" to "${ctx.config.domain}:blocks/${block.id}"
+                        jsonMark,
+                        "parent" to "${ctx.config.domain}:block/${block.id}"
+                    ).toString()
+                }
+                modelJsonPath.parentFile.mkdirs()
+                modelJsonPath.writeText(modelJsonStr)
+            }
+
+            println("Writing blockstates...")
+            for (block in blocks) {
+                val jsonPath = File(ctx.assetsRootDir, "blockstates/${block.id}.json")
+                val json  = when {
+                    block.blockStates != null -> block.blockStates.toJsonObject()
+                    else -> jsonObject(
+                        "variants" to jsonObject(
+                            "normal" to jsonObject(
+                                "model" to "${ctx.config.domain}:${block.id}"
+                            )
                         )
                     )
                 }
-            }
-            modelJson.put(jsonMark)
-            modelJsonPath.parentFile.mkdirs()
-            modelJsonPath.writeText(modelJson.toString())
-        }
 
-        println("Writing block item models...")
-        for (block in blocksWithItemBlock) if (block.itemBlock.generateModel) {
-            val modelJsonPath = File(ctx.assetsRootDir, "models/item/${block.id}.json")
-            val modelJsonStr = if (block.itemBlock.model != null) {
-                val obj: JsonObject = gson.fromJson(block.itemBlock.model.root().render(ConfigRenderOptions.concise()))
-                obj[jsonMark.first] = jsonMark.second
-                obj.toString()
-            } else {
-                jsonObject(
-                    jsonMark,
-                    "parent" to "${ctx.config.domain}:block/${block.id}"
-                ).toString()
-            }
-            modelJsonPath.parentFile.mkdirs()
-            modelJsonPath.writeText(modelJsonStr)
-        }
-
-        println("Writing blockstates...")
-        for (block in blocks) {
-            val jsonPath = File(ctx.assetsRootDir, "blockstates/${block.id}.json")
-            val json  = when {
-                block.blockStates != null -> block.blockStates.toJsonObject()
-                else -> jsonObject(
-                    "variants" to jsonObject(
-                        "normal" to jsonObject(
-                            "model" to "${ctx.config.domain}:${block.id}"
-                        )
-                    )
-                )
+                json += jsonMark
+                jsonPath.parentFile.mkdirs()
+                jsonPath.writeText(json.toString())
             }
 
-            json += jsonMark
-            jsonPath.parentFile.mkdirs()
-            jsonPath.writeText(json.toString())
-        }
-
-        println("Writing block ext models...")
-        for (block in blocks) {
-            if (block.extModels != null) {
-                for (m in block.extModels.root()) {
-                    val path = File(ctx.assetsRootDir, "models/block/${m.key}.json")
-                    val conf = (m.value as ConfigObject).withValue(jsonMark.first, ConfigValueFactory.fromAnyRef(jsonMark.second))
-                    path.writeText(conf.render(ConfigRenderOptions.concise()))
+            println("Writing block ext models...")
+            for (block in blocks) {
+                if (block.extModels != null) {
+                    for (m in block.extModels.root()) {
+                        val path = File(ctx.assetsRootDir, "models/block/${m.key}.json")
+                        val conf = (m.value as ConfigObject).withValue(jsonMark.first, ConfigValueFactory.fromAnyRef(jsonMark.second))
+                        path.writeText(conf.render(ConfigRenderOptions.concise()))
+                    }
                 }
             }
-        }
 
-        println("Writing blocks class...")
-        run {
-            val blocksClassFile = File(ctx.srcDir, ctx.config.blocksClassPath.replace('.', '/') + ".java")
-            blocksClassFile.parentFile.mkdirs()
+            println("Writing blocks class...")
+            run {
+                val blocksClassFile = File(ctx.srcDir, cfg.classPath.replace('.', '/') + ".java")
+                blocksClassFile.parentFile.mkdirs()
 
-            val s = renderTemplate("blockClassTemplate.java", mapOf(
-                "date" to dateFormat.format(Date()),
-                "config" to ctx.config,
-                "blocks" to blocks,
-                "blocksWithItemBlock" to blocksWithItemBlock
-            ))
-            blocksClassFile.writeText(s)
+                val s = renderTemplate("blockClassTemplate.java", mapOf(
+                    "date" to dateFormat.format(Date()),
+                    "config" to ctx.config,
+                    "blocks" to blocks,
+                    "blocksWithItemBlock" to blocksWithItemBlock,
+                    "annotations" to cfg.classAnnotations,
+                    "packageName" to cfg.classPath.substringBeforeLast('.'),
+                    "className" to cfg.classPath.substringAfterLast('.')
+                ))
+                blocksClassFile.writeText(s)
+            }
         }
     }
 
